e580da02de82a656bec9a0c5b65f372da895788e
Julian Tellez <juliantellezmendez@gmail.com>
2019-01-01 23:56:19 +0000

    constants: create pluginlifecycle

---
+++ src/Constants/PluginLifeCycle.ts
@@ -0,0 +1,25 @@
+enum PreHandlerLifeCycleHooks {
+    ON_REQUEST = 'onRequest',
+    ON_AUTH = 'onAuth',
+    ON_PRE_HANDLER = 'onPreHandler'
+}
+
+enum PostHandlerLifeCycleHooks {
+    ON_POST_HANDLER = 'onPostHandler',
+    ON_PRE_RESPONSE = 'onPreResponse'
+}
+
+enum PluginLifeCycleHooks {
+    ON_REQUEST = 'onRequest',
+    ON_AUTH = 'onAuth',
+    ON_PRE_HANDLER = 'onPreHandler',
+    ON_POST_HANDLER = 'onPostHandler',
+    ON_PRE_RESPONSE = 'onPreResponse',
+    ON_ERROR = 'onError'
+}
+
+export {
+    PluginLifeCycleHooks,
+    PreHandlerLifeCycleHooks,
+    PostHandlerLifeCycleHooks
+};
