3b60c19a53ba4fe4b5dcecae0d0a510407eb1f6b
Julian Tellez <juliantellezmendez@gmail.com>
2019-01-01 21:04:36 +0000

    scafold: add webpack config

---
+++ config/webpack/webpack.config.ts
@@ -0,0 +1,32 @@
+import * as path from 'path';
+
+const ENTRY_FILE = path.resolve(__dirname, '../../src/main.ts')
+const OUTPUT_DIR = path.resolve(__dirname, '../../dist')
+
+
+const webpackConfig = {
+    mode: 'production',
+    entry: ENTRY_FILE,
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js",
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts']
+    },
+    optimization: {
+        minimize: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts)?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
+}
+
+export default webpackConfig;
